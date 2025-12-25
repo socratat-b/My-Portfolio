eb6d35fe64922096ecf5197dfcf2544d4696ad18
tat <bedisscottandrew@gmail.com>
2025-12-25 20:44:29 +0800

    fix layout of the sidebar for bigger screen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 export default function SideBar() {
   return (
-    <aside className="hidden lg:block sticky h-dvh inset-y-0 left-0 w-72 border border-black">
-      <h1>Test</h1>
+    <aside className="hidden lg:flex flex-col items-center py-6 sticky h-dvh inset-y-0 left-0 w-56 border border-black ">
+      <Link
+        to={"/"}
+        className="font-black tracking-tighter text-2xl mb-8 block"
+      >
+        Tat
+      </Link>
+
+      <nav>
+        <ul className="space-y-4">
+          <li>
+            <Link className="text-lg tracking-tight font-medium" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="text-lg tracking-tight font-medium" to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link className="text-lg tracking-tight font-medium" to="/">
+              CV
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </aside>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,7 @@
 export default function HomePage() {
   return (
-    <main className="flex-1 border border-black">
-      <div className="h-screen">1</div>
-      <div className="h-screen">2</div>
-      <div className="h-screen">3</div>
-      <div className="h-screen">4</div>
-      <div className="h-screen">5</div>
-      <div className="h-screen">6</div>
+    <main className="flex-1 lg:py-6 px-6 border border-black min-h-screen">
+      <h1 className="">Welcome!</h1>
     </main>
   );
 }
