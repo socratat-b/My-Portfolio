f803eb1e2862141733f9da854ef32e4d593d3acf
tat <bedisscottandrew@gmail.com>
2025-12-26 22:27:33 +0800

    added simple homepage texts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center px-6 py-4 md:px-12 md:py-6 lg:hidden sticky top-0">
+    <header className="flex backdrop-blur-xs justify-between items-center px-6 py-4 md:px-12 md:py-6 lg:hidden sticky top-0">
       <Link to={"/"} className="font-black tracking-tighter text-2xl">
         Tat
       </Link>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 export default function HomePage() {
   return (
-    <main className="flex-1 lg:py-6 px-6 border border-black min-h-screen">
-      <h1 className="">Welcome!</h1>
+    <main className="flex-1 lg:py-6 px-6 py-4 border border-black min-h-screen">
+      <h1 className="font-semibold tracking-tighter text-xl text-black/75 pb-4">
+        Hello there, Welcome!
+      </h1>
+
+      <p className="text-4xl tracking-tighter font-black text-black/75 text-pretty leading-14">
+        I am Scott Andrew T. Bedis a fullstack developer based on Albay,
+        Philippines.
+      </p>
     </main>
   );
 }
